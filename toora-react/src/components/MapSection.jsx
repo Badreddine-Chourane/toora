726d6b8f3b4cb2b20b5dcdb1dd34a49818384264
Aya Hafri <115592428+hafriaya@users.noreply.github.com>
2025-05-02 16:00:59 +0100

    add_map

--- toora-react/src/components/MapSection.jsx
+++ toora-react/src/components/MapSection.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix for default marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const MapSection = () => {
+  const scooters = [
+    { id: 1, latitude: 33.5731, longitude: -7.5898, ville: "Casablanca" },
+    { id: 2, latitude: 34.0209, longitude: -6.8416, ville: "Rabat" },
+    { id: 3, latitude: 31.6295, longitude: -7.9811, ville: "Marrakech" },
+  ];
+
   return (
     <section className="py-5 bg-white">
       <div className="container">
@@ -17,13 +34,26 @@ const MapSection = () => {
           </div>
           <div className="col-md-6">
             <div className="ratio ratio-16x9">
-              <div className="bg-light rounded d-flex align-items-center justify-content-center">
-                <div className="text-center p-4">
-                  <i className="bi bi-map text-muted display-4 mb-3"></i>
-                  {/* <p className="text-muted">Carte interactive des trottinettes disponibles</p> */}
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-              </div>
+              <MapContainer
+                center={[33.5731, -7.5898]} // Default center (Casablanca)
+                zoom={8}
+                style={{ height: "100%", width: "100%" }}
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {scooters.map((scooter) => (
+                  <Marker
+                    key={scooter.id}
+                    position={[scooter.latitude, scooter.longitude]}
+                  >
+                    <Popup>
+                      Scooter disponible à {scooter.ville}
+                    </Popup>
+                  </Marker>
+                ))}
+              </MapContainer>
             </div>
           </div>
         </div>
